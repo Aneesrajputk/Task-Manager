f48ae5326cfa0f5127a723549c9ce0a6afcf4b95
Anees Rajput <118380093+Aneesrajputk@users.noreply.github.com>
2025-07-29 00:10:19 +0530

    Update Login.jsx

--- Frontend/Task-Manger/src/pages/Auth/Login.jsx
+++ Frontend/Task-Manger/src/pages/Auth/Login.jsx
@@ -1,8 +1,12 @@
 import React,{useState} from 'react'
 import Input from "../../components/Inputs/Input"
 import AuthLayout from  "../../components/layouts/AuthLayout"
-
+import { Link } from 'react-router-dom';
 import {useNavigate} from "react-router-dom";
+import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+ import  {API_PATHS} from '../../utils/apiPaths'
+
 const Login = () => {
   const [email,setEmail]=useState("");
   const [password,setpassword]=useState("");
@@ -12,6 +16,39 @@ const Login = () => {
   // Handle Login Form Submit
   const handleLogin=async(e)=>{
     e.preventDefault();
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if(!password){
+       setError("Please enter the password.");
+      return;
+    }
+    setError(""); 
+     
+    // Login api call
+    try{
+      const response =await axiosInstance.post(API_PATHS.AUTH.LOGIN,{
+       email,
+       password,
+      });
+      const {token,role}=response.data;
+      if(token){
+        localStorage.setItem("token",token);
+        //  redirect base on role
+        if(role==="admin"){
+          navigate("/admin/dashboard");
+        }else{
+          navigate("/user/dashboard");
+        }
+      }
+    }catch(error){
+      if(error.response && error.response.data.message){
+        setError(error.response.data.message);
+      }else{
+        setError("Something went wrong .please try again.");
+      }
+    }
   };
 
 
@@ -28,6 +65,23 @@ const Login = () => {
       placeholder="Kaju@example.com"
       type="text"
       />
+
+      <Input 
+      value={password}
+      onChange={({target})=>setpassword(target.value)}
+      label="Password"
+      placeholder="Min 8 Characters"
+      type="password"
+      />
+      {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+      <button type="submit" className='btn-primary'>LOGIN</button>
+      <p className='text-[13px] text-slate-800 mt-3'>
+        Don't have an account?{" "}
+        <Link className="font-medium text-primary underline" to="/signup">SignUp</Link>
+      </p>
+
+
+
      </form>
     </div>
   </AuthLayout>
